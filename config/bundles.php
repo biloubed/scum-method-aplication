d77d5e6aa8c8b635fc7f6bef0b0130564fc92668
bilal bedine <bilal.bedine@devinci.fr>
2019-02-20 12:32:42 +0100

    rest

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
